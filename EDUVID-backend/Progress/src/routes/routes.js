533344c5dd8f61a31af9ca9ca996b705a31a5755
juan_pab_pds_gmz <Juan_pab.paredes@uao.edu.co>
2025-09-25 16:37:02 -0500

    Ajustes en routes.js después de merge

--- EDUVID-backend/Progress/src/routes/routes.js
+++ EDUVID-backend/Progress/src/routes/routes.js
@@ -4,9 +4,6 @@ import { completeLesson, getprogress } from "../controllers/controllers.js";
 const router = Router();
 
 router.post("/complete-lesson", completeLesson);
-
 router.get("/", getprogress);
-
-
 
 export default router;
