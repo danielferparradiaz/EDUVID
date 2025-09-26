1a6f68cfeff2843690fc6a5322df0bef0641efc8
Daniel <Danielferparradiaz@gmail.com>
2025-09-26 00:35:05 -0500

    se actualiza courses

---
+++ EDUVID-backend/certificates/src/config/eureka.js
@@ -0,0 +1,33 @@
+import { Eureka } from "eureka-js-client";
+
+const eurekaClient = new Eureka({
+  instance: {
+    app: "CERTIFICATES-SERVICE",
+    hostName: "localhost",
+    ipAddr: "127.0.0.1",
+    port: {
+      $: 8085,
+      "@enabled": true,
+    },
+    vipAddress: "CERTIFICATES-SERVICE",
+    dataCenterInfo: {
+      "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+      name: "MyOwn",
+    },
+  },
+  eureka: {
+    host: "localhost",
+    port: 8761,
+    servicePath: "/eureka/apps/",
+  },
+});
+
+eurekaClient.start((error) => {
+  if (error) {
+    console.error("❌ Error registrando CERTIFICATES-SERVICE en Eureka:", error);
+  } else {
+    console.log("✅ CERTIFICATES-SERVICE registrado en Eureka");
+  }
+});
+
+export default eurekaClient;
